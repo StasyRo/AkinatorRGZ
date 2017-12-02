8db19722e65c474fea1c29278af92c8cf51cb07d
StasyRo <32781870+StasyRo@users.noreply.github.com>
2017-12-02 15:33:55 +0200

    Создание персонажей

--- Characters.java
+++ Characters.java
@@ -1,4 +1,11 @@
 package com.company;
+import java.util.Scanner;
+
 
 public class Characters {
+    public String[] Aladin = {"1", "0", "0", "0", "Aladin"}; //woman - 0 man - 1; unreal - 0, real - 1; tale - 0, film - 1; inlove - 0, not - 1
+    public String[] Shapokliak = {"0", "0", "0", "1", "Shapokliak"}; //woman - 0 man - 1; unreal - 0, real - 1; tale - 0, film - 1; inlove - 0, not - 1
+    public String[] Alise = {"0", "0", "1", "1", "Alise"}; //woman - 0 man - 1; unreal - 0, real - 1; tale - 0, film - 1; inlove - 0, not - 1
+    public String[] Getsby = {"1", "1", "1", "0", "Getsby"}; //woman - 0 man - 1; unreal - 0, real - 1; tale - 0, film - 1; inlove - 0, not - 1
+    public String [] Buratino = {"1", "0", "0", "0", "Buratino"};
 }

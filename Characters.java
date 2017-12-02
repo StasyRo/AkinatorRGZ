e7b9ee1f508a417c460c6041fa96b2e8c4608fb7
StasyRo <32781870+StasyRo@users.noreply.github.com>
2017-12-02 15:40:49 +0200

    Заполнение метода StrangeCharacter();

--- Characters.java
+++ Characters.java
@@ -10,6 +10,45 @@ public class Characters {
     public String [] Buratino = {"1", "0", "0", "0", "Buratino"};
 
     public void StrangeCharacter() {
-      
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Man-1, Woman-0");
+        String a = sc.nextLine();
+        System.out.println(a);
+        System.out.println("Real-1, Unreal-0");
+        String b = sc.nextLine();
+        System.out.println("Film-1, Tale-0");
+        String c = sc.nextLine();
+        System.out.println("Not inlove-1, Inlove-0");
+        String d = sc.nextLine();
+        String[] A = {a, b, c, d};
+        int flagA = 0, flagB=0, flagC=0, flagD=0, flagBr = 0;
+        for (int m = 0; m < 4; m++) {
+            if (A[m].equals(Buratino[m])) {
+                flagBr += 1;
+            }
+            if (A[m].equals(Aladin[m])) {
+                flagA += 1;
+            }
+            if (A[m].equals(Shapokliak[m])) {
+                flagB += 1;
+            }
+            if (A[m].equals(Alise[m])) {
+                flagC += 1;
+            }
+            if (A[m].equals(Getsby[m])) {
+                flagD += 1;
+            }
+        }
+        if (flagA > 3) System.out.println("It's " + Aladin[4] + " :))");
+        else if(flagB > 3) System.out.println("It's " + Shapokliak[4] + " :))");
+        else if(flagC > 3) System.out.println("It's " + Alise[4] + " :))");
+        else if(flagD > 3) System.out.println("It's " + Getsby[4] + " :))");
+        else if(flagBr > 3) System.out.println("It's " + Buratino[4] + " :))");
+        else{ System.out.println("I don't know :((");
+            System.out.println("Who is it? Told me name, I'll remember it!");
+            Scanner sc2 = new Scanner(System.in);
+            String newCharacter = sc2.nextLine();
+            System.out.println("Ohh, it's " + newCharacter);
+        }
     }
 }
